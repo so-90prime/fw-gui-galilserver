a3a4182d1180b0bd4f4a227dd813d9fbfcb5b0f9
davner <davner@email.arizona.edu>
2019-11-18 22:23:54 -0700

    starting tools

--- src/galilexample.py
+++ src/galilexample.py
@@ -1,13 +1,45 @@
 import socket
 import sys
 from struct import *
+import cmd
 
 HOST = '10.30.1.2'
 PORT = 9874
 
 
-def test()
+class GalilTools(cmd.Cmd):
+    """Engineering Tools to Debug Galil Issues"""
+    def preloop(self):
+        self.host = HOST
+        self.port = PORT
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(5)
+
+    def do_connect(self, line):
+        'Connect to galilserver'
+        try:
+            self.sock.connect((self.host, self.port))
+            self.do_receive()
+        except Exception as err:
+            print('Error: {}'.format(err))
+
+    def do_receive(self, line):
+        'Receive response from galilserver'
+        message = self.sock.recv(size).decode()
+        print(message)
+
+    def do_unpack(self, size=1024, data_type='h'):
+        'Unpack binary data stream'
+        message = self.sock.recv(size)
+
+    def do_show_all_lvdt_values(self, line):
+        'Return all LVDT values'
+        print('test')
+
+
+def test():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((self.host, self.port))
     # returns "OK: Connected to galilserver. Client 1"
     message = sock.recv(1024).decode()
     prints(message)
@@ -24,7 +56,6 @@ def test()
     message = sock.recv(1024)
     sock.close()
 
-
     # must remove the "OK: " of response
     message_slice = message[4:len(message)]
     # each actuator encoder value is at different byte locations
@@ -36,7 +67,11 @@ def test()
     # must unpack the response
     # 'h' is for signed short integer
     # https://docs.python.org/3.8/library/struct.html
-    actuator_encoder_a = unpack('h', raw_actuator_encoder_a)[0] # returns tuple
+    actuator_encoder_a = unpack('h',
+                                raw_actuator_encoder_a)[0]  # returns tuple
     prints('Actuator Encoder A'.format(actuator_encoder_a))
     return None
 
+
+if __name__ == '__main__':
+    GalilTools().cmdloop()
